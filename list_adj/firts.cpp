5654144ec89d036f7285428c6f2166762fec21f9
ubuangel <andresbjar97@gmail.com>
2020-08-18 19:12:48 -0500

    empesando

---
+++ list_adj/firts.cpp
@@ -0,0 +1,21 @@
+/*
+ * firts.cpp
+ *
+ *  Created on: 18 ago. 2020
+ *      Author: angel
+ */
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+int main(){
+	
+	cout<< <<endl;
+	//cout<< <<"\n";
+
+	}
+
+
+
